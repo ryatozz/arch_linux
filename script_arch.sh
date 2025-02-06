50bfe456919f781f11d09b6bbcaf51772ce4b82b
ryatozz <151998545+ryatozz@users.noreply.github.com>
2025-02-07 00:21:18 +0100

    Update script_arch.sh v5
    Update script_arch.sh v5

--- script_arch.sh
+++ script_arch.sh
@@ -2,19 +2,18 @@
 
 set -e
 
+###############################################################################
+# Variables
+###############################################################################
 DISK="/dev/sda"
 ENC_DISK="/dev/sdb"
 HOSTNAME="archlinux"
 USERNAME="ryatozz"
+PASSWORD="azerty123"   # Mot de passe pour root et user
 
-echo "[*] Vérification et démontage des partitions..."
-mount | grep "$DISK" | awk '{print $3}' | xargs -r umount -l
-swapoff -a
-
-echo "[*] Suppression de l'ancienne table de partitions..."
-wipefs -a "$DISK"
-sgdisk --zap-all "$DISK"
-
+###############################################################################
+# 1. Partitionnement du disque principal (pour /)
+###############################################################################
 echo "[*] Partitionnement du disque principal..."
 parted -s "$DISK" mklabel gpt
 parted -s "$DISK" mkpart ESP fat32 1MiB 512MiB
@@ -27,74 +26,135 @@ mount "${DISK}2" /mnt
 mkdir /mnt/boot
 mount "${DISK}1" /mnt/boot
 
+###############################################################################
+# 2. Chiffrement du disque secondaire + LVM dessus
+###############################################################################
 echo "[*] Chiffrement du disque secondaire..."
-echo -n "archlinux" | cryptsetup luksFormat "$ENC_DISK" --type luks1
-echo "archlinux" | cryptsetup open "$ENC_DISK" cryptroot 
+# Mot de passe en clair "azerty123" juste pour l'exemple
+echo -n "$PASSWORD" | cryptsetup luksFormat "$ENC_DISK" -
+echo -n "$PASSWORD" | cryptsetup open "$ENC_DISK" cryptroot
 
+# Création du PV LVM, du VG et des LVs
 pvcreate /dev/mapper/cryptroot
 vgcreate vg0 /dev/mapper/cryptroot
-lvcreate -L 5G vg0 -n shared_folder
+lvcreate -L 10G vg0 -n storage
+lvcreate -L 5G vg0 -n share
 
-mkfs.ext4 /dev/vg0/shared_folder
+mkfs.ext4 /dev/vg0/storage
+mkfs.ext4 /dev/vg0/share
 
-mkdir /mnt/shared_folder
-mount /dev/vg0/shared_folder /mnt/shared_folder
+mkdir /mnt/storage
+mkdir /mnt/share
+mount /dev/vg0/storage /mnt/storage
+mount /dev/vg0/share /mnt/share
 
-echo "[*] Installation de base..."
-pacstrap /mnt base linux linux-firmware lvm2 sudo vim git wget gcc make gdb base-devel virtualbox virtualbox-host-modules-arch
+###############################################################################
+# 3. Installation de base
+###############################################################################
+echo "[*] Installation de base (pacstrap) ..."
+pacstrap /mnt base linux linux-firmware lvm2 sudo vim git wget \
+  gcc make gdb base-devel \
+  virtualbox virtualbox-host-modules-arch
 
-mkdir -p /mnt/proc /mnt/sys /mnt/dev /mnt/dev/pts
-
+# Génération du fstab
 genfstab -U /mnt >> /mnt/etc/fstab
 
+###############################################################################
+# 4. Configuration en chroot
+###############################################################################
 arch-chroot /mnt /bin/bash <<EOF
+  # ------------------------------------------------
+  # Configuration de base (locales, hostname, user)
+  # ------------------------------------------------
   echo "$HOSTNAME" > /etc/hostname
-  hostnamectl set-hostname $HOSTNAME
+  hostnamectl set-hostname "$HOSTNAME"
   ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
   hwclock --systohc
+
   echo "LANG=fr_FR.UTF-8" > /etc/locale.conf
   echo "KEYMAP=fr" > /etc/vconsole.conf
+  sed -i 's/^#fr_FR.UTF-8 UTF-8/fr_FR.UTF-8 UTF-8/' /etc/locale.gen
   locale-gen
 
-  useradd -m -G wheel -s /bin/bash $USERNAME
-  echo "$USERNAME:azerty123" | chpasswd
-  echo "root:azerty123" | chpasswd
+  # Création de l'utilisateur
+  useradd -m -G wheel -s /bin/bash "$USERNAME"
+  echo "$USERNAME:$PASSWORD" | chpasswd
+  echo "root:$PASSWORD" | chpasswd
+
   echo "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/wheel
 
+  # ------------------------------------------------
+  # Installation de l'environnement graphique
+  # ------------------------------------------------
   pacman -Syu --noconfirm xorg-server xorg-xinit sddm hyprland alacritty neovim firefox
 
+  # Configuration Hyprland
   mkdir -p /home/$USERNAME/.config/hypr
   cat <<EOT > /home/$USERNAME/.config/hypr/hyprland.conf
 monitor=,preferred,auto,1
 exec-once=alacritty
 input {
-  kb_layout=fr
-  follow_mouse=1
+    kb_layout=fr
+    follow_mouse=1
 }
 general {
-  gaps_in=5
-  gaps_out=10
-  border_size=2
-  col.active_border=0xff8aadf4
-  col.inactive_border=0xff1a1b26
+    gaps_in=5
+    gaps_out=10
+    border_size=2
+    col.active_border=0xff8aadf4
+    col.inactive_border=0xff1a1b26
 }
 EOT
   chown -R $USERNAME:$USERNAME /home/$USERNAME/.config/hypr
 
+  # Activer le gestionnaire de session SDDM
   systemctl enable sddm
 
+  # ------------------------------------------------
+  # Configuration du dossier partagé + Samba
+  # ------------------------------------------------
   groupadd partage
   usermod -aG partage $USERNAME
-  mkdir /home/$USERNAME/shared_folder
-  chown -R $USERNAME:partage /home/$USERNAME/shared_folder
-  chmod -R 770 /home/$USERNAME/shared_folder
+  # Petit dossier "share" dans le home
+  mkdir /home/$USERNAME/share
+  chown -R $USERNAME:partage /home/$USERNAME/share
+  chmod -R 770 /home/$USERNAME/share
 
   pacman -S --noconfirm samba
-  echo "[shared_folder]" >> /etc/samba/smb.conf
-  echo "path = /home/$USERNAME/shared_folder" >> /etc/samba/smb.conf
+  echo "[share]" >> /etc/samba/smb.conf
+  echo "path = /home/$USERNAME/share" >> /etc/samba/smb.conf
   echo "guest ok = yes" >> /etc/samba/smb.conf
   systemctl enable smb
+
+  # ------------------------------------------------
+  # Installation et configuration du bootloader (GRUB)
+  # ------------------------------------------------
+  pacman -S --noconfirm grub efibootmgr virtualbox-guest-utils
+
+  # Installation de grub en UEFI
+  grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+  grub-mkconfig -o /boot/grub/grub.cfg
+
+  # (Optionnel) Mise à jour de l’initramfs au cas où
+  # mkinitcpio -P
+
+  # ------------------------------------------------
+  # Activer le service VirtualBox Guest (pour la VM invitée)
+  # ------------------------------------------------
+  systemctl enable vboxservice
+
+  # ------------------------------------------------
+  # Installation et activation d'un gestionnaire réseau
+  # (recommandé pour avoir le net en interface graphique)
+  # ------------------------------------------------
+  pacman -S --noconfirm networkmanager
+  systemctl enable NetworkManager
+
 EOF
 
-echo "[*] Installation terminée ! Redémarrage..."
+###############################################################################
+# 5. Fin de l'installation
+###############################################################################
+echo "[*] Installation terminée ! Redémarrage dans 10 secondes..."
+sleep 10
 reboot
