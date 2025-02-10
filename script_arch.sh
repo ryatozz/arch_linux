d3d18245b9ee13b0b0f5a3fbfd63be622eaefe37
ryatozz <151998545+ryatozz@users.noreply.github.com>
2025-02-10 21:57:49 +0100

    Update script_arch.sh v10
    Update script_arch.sh v10

--- script_arch.sh
+++ script_arch.sh
@@ -1,48 +1,52 @@
 #!/bin/bash
-
 set -e
 
-
 DISK="/dev/sda"
-ENC_DISK="/dev/sdb"
 HOSTNAME="archlinux"
 USERNAME="ryatozz"
 PASSWORD="azerty123"
 
-echo "[*] Partitionnement du disque principal (UEFI + partition racine)..."
-parted -s "$DISK" mklabel gpt
-parted -s "$DISK" mkpart ESP fat32 1MiB 512MiB
-parted -s "$DISK" set 1 esp on
-parted -s "$DISK" mkpart primary ext4 512MiB 100%
 
+echo "[*] Création de la table de partitions sur $DISK..."
+parted -s "$DISK" mklabel gpt
+
+
+parted -s "$DISK" mkpart ESP fat32 1MiB 513MiB
+parted -s "$DISK" set 1 esp on
+
+
+parted -s "$DISK" mkpart primary ext4 513MiB 30.5GiB
+
+
+parted -s "$DISK" mkpart primary ext4 30.5GiB 100%
+
+
+echo "[*] Formatage de la partition ESP et de la partition racine..."
 mkfs.fat -F32 "${DISK}1"
 mkfs.ext4 "${DISK}2"
+
 
 mount "${DISK}2" /mnt
 mkdir -p /mnt/boot
 mount "${DISK}1" /mnt/boot
 
-echo "[*] Chiffrement du deuxième disque ($ENC_DISK) + LVM..."
 
-echo -n "$PASSWORD" | cryptsetup luksFormat "$ENC_DISK" -
-echo -n "$PASSWORD" | cryptsetup open "$ENC_DISK" cryptroot
-
+echo "[*] Chiffrement de ${DISK}3 et configuration LVM..."
+echo -n "$PASSWORD" | cryptsetup luksFormat "${DISK}3" -
+echo -n "$PASSWORD" | cryptsetup open "${DISK}3" cryptroot
 
 pvcreate /dev/mapper/cryptroot
 vgcreate vg0 /dev/mapper/cryptroot
 
 
 lvcreate -L 10G vg0 -n storage
-
 lvcreate -L 5G vg0 -n share
-
 lvcreate -L 16G vg0 -n swap
 
 
 mkfs.ext4 /dev/vg0/storage
 mkfs.ext4 /dev/vg0/share
 mkswap /dev/vg0/swap
-
 
 
 echo "[*] Installation de base (pacstrap)..."
@@ -52,22 +56,21 @@ pacstrap /mnt \
   gcc make gdb base-devel \
   virtualbox virtualbox-host-modules-arch
 
+
 echo "[*] Génération du fstab..."
 genfstab -U /mnt >> /mnt/etc/fstab
 
 
-
 cat <<EOF >> /mnt/etc/fstab
 
-# Volumes chiffrés (ouvert et monté manuellement) :
-
+# Volumes chiffrés (à ouvrir et monter manuellement) :
 #/dev/vg0/storage   /storage   ext4 defaults,noauto 0 0
 #/dev/vg0/share     /share     ext4 defaults,noauto 0 0
 #/dev/vg0/swap      none       swap defaults,noauto 0 0
 EOF
 
+### Chroot et configuration de base
 arch-chroot /mnt /bin/bash <<EOFCHROOT
-
   echo "[*] Configuration de base..."
   echo "$HOSTNAME" > /etc/hostname
   hostnamectl set-hostname "$HOSTNAME"
@@ -90,7 +93,7 @@ arch-chroot /mnt /bin/bash <<EOFCHROOT
   ###########################################################################
   pacman -Syu --noconfirm xorg-server xorg-xinit sddm hyprland alacritty neovim firefox
 
-  # Configuration Hyprland pour l'utilisateur
+  # Configuration de Hyprland pour l'utilisateur
   mkdir -p /home/$USERNAME/.config/hypr
   cat <<EOT > /home/$USERNAME/.config/hypr/hyprland.conf
 monitor=,preferred,auto,1
@@ -109,7 +112,7 @@ general {
 EOT
   chown -R $USERNAME:$USERNAME /home/$USERNAME/.config/hypr
 
-  # Activer le gestionnaire de session SDDM
+  # Activer SDDM pour la session graphique
   systemctl enable sddm
 
   ###########################################################################
@@ -123,9 +126,11 @@ EOT
   chmod -R 770 /home/$USERNAME/share
 
   pacman -S --noconfirm samba
-  echo "[share]" >> /etc/samba/smb.conf
-  echo "path = /home/$USERNAME/share" >> /etc/samba/smb.conf
-  echo "guest ok = yes" >> /etc/samba/smb.conf
+  cat <<EOSamba >> /etc/samba/smb.conf
+[share]
+path = /home/$USERNAME/share
+guest ok = yes
+EOSamba
   systemctl enable smb
 
   ###########################################################################
@@ -141,7 +146,6 @@ EOT
   ###########################################################################
   pacman -S --noconfirm networkmanager
   systemctl enable NetworkManager
-
 EOFCHROOT
 
 echo "[*] Installation terminée ! Redémarrage dans 10 secondes..."
